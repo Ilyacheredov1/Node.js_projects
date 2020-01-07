a7e3b65c4d93d1816ad9a64e173168732bd690e1
Ilyacheredov1 <Ilyacheredov@gmail.com>
2020-01-07 21:53:38 +0300

    init max_app

--- Weather_app/test.ts
+++ Weather_app/test.ts
@@ -1,5 +1,7 @@
-const test: number = 1 + 1
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+};
 
+const mergedObj = merge(30, 30)
 
-
-console.log(test);
+console.log(mergedObj);
